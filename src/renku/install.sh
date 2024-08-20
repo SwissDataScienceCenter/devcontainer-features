e83173566b286e5c41e5148147a4cf0aeb946caf
Rok Roškar <rokroskar@gmail.com>
2024-08-20 16:15:18 +0200

    chore: install nightly build

--- src/renku/install.sh
+++ src/renku/install.sh
@@ -4,14 +4,6 @@ set -ex
 USER_ID=1000
 USERNAME=${USERNAME:-${_REMOTE_USER}}
 
-echo "Activating feature Renku"
-
-VERSION=${VERSION:-"latest"}
-if [ "${VERSION}" = "latest" ]; then
-    VERSION=$(curl -s https://pypi.org/pypi/renku/json | jq '.releases | keys | sort | .[]'  | grep -oP "\"[0-9]+\.[0-9]+\.[0-9]\"" | tr -d '"' | sort -rV | head -n 1)
-fi
-echo "The requested version is: $VERSION"
-
 # create the user if missing
 if ! id -u "${USERNAME}" >/dev/null 2>&1 && [ "$CREATEUSER" = "true" ]; then
     useradd -l -m -s /bin/bash -N -u 1000 "${USERNAME}"
@@ -24,6 +16,6 @@ if [ "${INSTALLJUPYTER}" = "true" ]; then
 fi
 
 # install the renku CLI
-curl -sfSL https://raw.githubusercontent.com/SwissDataScienceCenter/renku-cli/main/install.sh | bash
+curl -sfSL https://raw.githubusercontent.com/SwissDataScienceCenter/renku-cli/main/install.sh | bash -s -- -t nightly
 
 chown -R ${USERNAME} /usr/local/py-utils
